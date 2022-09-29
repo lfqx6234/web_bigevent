7198a4ae102ee167fff0f0e568718300808cb4d2
chenlingfeng <1270120891@qq.com>
2022-09-29 23:47:37 +0800

    后台页面

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,6 +1,21 @@
+//过滤器的作用，在发送前会先截取到信息
 $.ajaxPrefilter(function(options) {
     // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
 
     options.url = 'http://www.liulongbin.top:3007' + options.url;
-    console.log(options.url);
+
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    options.complete = function(res) {
+        // console.log(res);
+        var res = res.responseJSON;
+        if (res.status === 1 || res.message === '身份认证失败！') {
+            localStorage.removeItem('token');
+            location.href = './login.html';
+        }
+    }
+
 })
---
+++ assets/js/index.js
@@ -0,0 +1,48 @@
+$(function() {
+    Getdata();
+
+    $('.layui-icon').on('click', function() {
+
+        layui.layer.confirm('是否要退出', { icon: 3, title: '提示' },
+            function(index) {
+                // location.removeItem = '';
+                localStorage.removeItem('token');
+                location.href = "login.html";
+                // console.log(index);
+                layer.close(index);
+            });
+    })
+})
+
+//封装 获取数据
+function Getdata() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success: function(res) {
+            if (res.status !== 0) {
+                return layer.msg('上传失败');
+            }
+            // console.log(res);
+            xravatar(res);
+        }
+
+    })
+};
+
+//渲染头像
+function xravatar(res) {
+    var name = res.data.nickname || res.data.username;
+    $('.welcome').html(name);
+    // console.log(name);
+    var first = name[0].toUpperCase();
+    if (res.data.user_pic === null) {
+        $('.layui-nav-img').hide();
+        $('.text-avatar').html(first).show();
+    }
+    $('.layui-nav-img').attr("src", res.data.user_pic);
+    $('.text-avatar').html(first).hide();
+};
